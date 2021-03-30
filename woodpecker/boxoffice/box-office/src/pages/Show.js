ffbbd61ad34b6ea18726337a1808fd0020268e37
Vedavathi GH <vedavathi.gh@gmail.com>
2021-03-30 19:16:03 +0530

    added show page

--- woodpecker/boxoffice/box-office/src/pages/Show.js
+++ woodpecker/boxoffice/box-office/src/pages/Show.js
@@ -1,21 +1,94 @@
-import React ,{useState,useEffect} from 'react'
+import React ,{useReducer,useEffect} from 'react'
 import {useParams} from 'react-router-dom';
 import { apiGet } from '../misc/config';
+import ShowMainData from '../components/show/ShowMainData';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import Cast from '../components/show/Cast';
+import { ShowPageWrapper, InfoBlock } from './Show.styled';
 
+
+const reducer=(prevState,action)=>{
+   switch(action.type){
+      case 'FETCH_SUCCESS':
+          {
+              return {
+                  isLoading:false,
+                  error:null,
+                  show:action.show
+              }
+          }
+          case 'FETCH_FAILED':{
+              return{
+                  ...prevState,
+                  isLaoding:false,
+                  error:action.error
+              }
+          }    
+
+       default:return prevState
+   }
+}
+const initialState={
+    show:null,
+    isLoading:true,
+    error:null
+}
 const Show = () => {
     const {id}=useParams();
-    const [show,setShow]=useState(null);
+  
+    //const [show,setShow]=useState(null);
+    //const [isLoading,setIsLoading]=useState(null);
+    //const [error,setError]=useState(null);
+    const [{show,isLoading,error},dispatch]=useReducer(reducer,initialState);
+    
     useEffect(()=>{
+        let isMounted=true;
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results=>{
-            setShow(results);
+           
+                if(isMounted){
+                    dispatch({type:'FETCH_SUCCESS',show:results})
+                   
+
+                }
+           
+           
+            
+        }).catch(err=>{
+            if(isMounted){
+                dispatch({type:'FETCH_FAILED',error:err.message})
+            }
         })
+        return ()=>{
+            isMounted=false;
+        }
     },[id]);
     console.log("show",show);
-     
+    console.log()
+     if(isLoading){
+       return <div>Data is being loaded</div>
+    }
+     if(error){
+     return <div>Error Occurred:{error}</div>
+     }
     return (
-        <div>
-            this is show page
-        </div>
+    <ShowPageWrapper>
+         <ShowMainData  image={show.image}  name={show.name} rating={show.rating} summary={show.summary} tags={show.genres}/>
+
+         <InfoBlock>
+         <h2>Details</h2>
+         <Details  status={show.status} network={show.network} premiered={show.premiered}/>
+         </InfoBlock >
+         <InfoBlock>
+         <h2>Seasons</h2>
+         <Seasons seasons={show._embedded.seasons}/>
+         </InfoBlock>
+         <InfoBlock>
+         <h2>Cast</h2>
+         <Cast cast={show._embedded.cast}/>
+         </InfoBlock>
+    </ShowPageWrapper>
+    
     )
 }
 
